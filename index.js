89914786af43b997f4b93d96b1ccb513c87cf70e
Anton Punko <ant.punko@gmail.com>
2018-09-06 13:43:33 +0300

    init electron app

---
+++ index.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: 'Price list diff manager',
+  });
+}
+
+app.on('ready', createWindow);
