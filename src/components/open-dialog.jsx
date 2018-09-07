ba1bc1d6b85da31ca06e362e430cf6b2e6c2c8c4
Anton Punko <ant.punko@gmail.com>
2018-09-07 15:28:33 +0300

    move path state to container

--- src/components/open-dialog.jsx
+++ src/components/open-dialog.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import * as Logger from 'electron-log';
 
 const { dialog } = require('electron').remote;
 
 class OpenDialog extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { path: null };
 
     this.handleClick = this.handleClick.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
@@ -28,19 +25,13 @@ class OpenDialog extends React.Component {
   }
 
   handleSelect(filePaths) {
-    Logger.info(filePaths);
-    this.setState({ path: filePaths ? filePaths[0] : null });
+    this.props.updatePath(this.props.name, filePaths ? filePaths[0] : null);
   }
 
   render() {
     return (
       <>
-        <span>
-          {this.props.label}
-          -
-          {this.state.path}
-        </span>
-        <button type="button" onClick={this.handleClick}>Select</button>
+        <button type="button" onClick={this.handleClick}>{this.props.label}</button>
       </>
     );
   }
@@ -48,6 +39,8 @@ class OpenDialog extends React.Component {
 
 OpenDialog.propTypes = {
   label: PropTypes.string.isRequired,
+  updatePath: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
   type: PropTypes.string,
 };
 
--- src/containers/app-container.jsx
+++ src/containers/app-container.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import OpenDialog from '../components/open-dialog';
 
 class AppContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      oldFilePath: null,
+      newFilePath: null,
+      outputDirectoryPath: null,
+    };
+
+    this.updatePath = this.updatePath.bind(this);
+  }
+
+  updatePath(name, path) {
+    this.setState({ [name]: path });
+  }
+
   render() {
     return (
       <>
-        <OpenDialog label="Select old file" />
-        <OpenDialog label="Select new file" />
-        <OpenDialog label="Select output directory" type="openDirectory" />
+        <OpenDialog label="Select old file" name="oldFilePath" updatePath={this.updatePath} />
+        <OpenDialog label="Select new file" name="newFilePath" updatePath={this.updatePath} />
+        <OpenDialog label="Select output directory" name="outputDirectoryPath" updatePath={this.updatePath} type="openDirectory" />
+        {this.state.oldFilePath}
+        {this.state.newFilePath}
+        {this.state.outputDirectoryPath}
       </>
     );
   }
